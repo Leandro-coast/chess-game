5ddb3020252004d7ffa78ed1676b6e4cb908db80
Leandro_coast <leandro_coast@outlook.com>
2022-06-29 21:30:45 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("test" +
-                "sss");
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
